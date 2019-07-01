d1702fe610c86deea577a8773319913130b7258b
Peter-Cohen <peter.cohen.1969@gmail.com>
2019-07-02 00:39:16 +0800

    Finished section "Advanced Templates"

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Homepage',
     welcomeMessage: 'Peter is heel lief. En mooi...',
-    currentYear: new Date().getFullYear(),
-  })
+  });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page',
-    currentYear: new Date().getFullYear(),  
   });
 });
 
 app.listen(3000, () => console.log('Server listening on port 3000'));
-
